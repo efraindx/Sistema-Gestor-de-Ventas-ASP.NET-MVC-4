a272656fd9c9a9d196b3bed232821a871c994afb
Efrain Toribio Reyes <efrainreyes05@hotmail.com>
2013-10-06 05:01:05 +0200

    first commit..

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Articulos mas destacados";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Página de descripción de la aplicación.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Página de contacto.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult FormularioMethod()
+        {
+            return View();        
+        }
+    }
+}
